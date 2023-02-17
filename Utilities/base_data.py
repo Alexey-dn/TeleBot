01d577a02ee0fbe7773b8e77386ceb081f2bcbea
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-02-17 16:19:23 +0300

    Created new file with base data

---
+++ Utilities/base_data.py
@@ -0,0 +1,14 @@
+TOKEN = "6292955501:AAFQmxTAMp2bJJtqsPb-qOTgledlayIKCfA"
+API_KEY = "80d8b5dfae3ebcefd487ea69"
+# API_KEY_old= "1Bmhwg8a8Jup31vR4HjvDpAznp9nULrj"
+
+cur_list = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    "Австралийский доллар": "AUD",
+    "Канадский доллар": "CAD",
+    "гривна": "UAH"
+
+}
+
